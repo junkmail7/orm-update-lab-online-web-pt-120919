1634349303b752ab269a9ecfca69697f7c6f0147
William Pannell <lpann5@gmail.com>
2020-07-02 23:41:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,9 +41,9 @@ class Student
     end
 
     def self.create(name:, grade:)
-      song = Song.new(name, grade)
-      song.save
-      song
+      student = Student.new(name, grade)
+      student.save
+      student
     end
 
     def self.drop_table
