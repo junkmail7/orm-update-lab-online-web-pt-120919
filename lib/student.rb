7ed2a625ff323064b445e1c0cd6250453c40b7c6
William Pannell <lpann5@gmail.com>
2020-07-02 23:30:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,13 +3,13 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :name, :grade
   attr_reader :id
-   
+
     def initialize(id=nil, name, grade)
       @id = id
       @name = name
       @grade = grade
     end
-   
+
     def self.create_table
       sql =  <<-SQL
         CREATE TABLE IF NOT EXISTS students (
@@ -20,23 +20,23 @@ class Student
           SQL
       DB[:conn].execute(sql)
     end
-   
+
     def save
       sql = <<-SQL
         INSERT INTO songs (name, grade)
         VALUES (?, ?)
       SQL
-   
+
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
     end
-   
+
     def self.create(name:, grade:)
       song = Song.new(name, grade)
       song.save
       song
     end
-   
+
     def self.find_by_name(name)
       sql = "SELECT * FROM songs WHERE name = ?"
       result = DB[:conn].execute(sql, name)[0]
